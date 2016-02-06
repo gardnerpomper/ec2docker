76b180871d87fce9474aff7cd37a3f44c40a8795
Gardner Pomper <gardner@networknow.org>
2016-02-06 18:12:38 -0500

    added scripts to create VNC desktop and removed references to personal info

--- ec2_desktop_setup.sh
+++ ec2_desktop_setup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 sudo bash -c 'echo "127.0.0.1 $(hostname)" >> /etc/hosts'
 sudo apt-get update
-#sudo apt-get upgrade x11-xserver-utils
 sudo apt-get install -y xfce4 vnc4server
 sudo apt-get install -y docker.io
 sudo usermod ubuntu -a -G docker
@@ -10,6 +9,4 @@ mv ec2_xstartup ~/.vnc/xstartup
 chmod +x ~/.vnc/xstartup
 echo -e "vnc1234\nvnc1234" | vncpasswd
 sudo su - ubuntu
-#vncserver
-#vncserver -kill :1
 vncserver -geometry 1600x1024 -geometry 1400x1050 -geometry 1280x1024 -geometry 1024x768
